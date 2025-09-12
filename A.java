fd8efe731a95cceba827920c8bad6981350e0d5e
srjampal <jampalsantosh90@gmail.com>
2025-09-12 09:12:51 +0530

    Add files via upload

--- A.java
+++ A.java
@@ -1,25 +1,14 @@
-package Array;
+package Practice;
+
+import java.util.Scanner;
 
 public class A {
-
+	
 	public static void main(String[] args) {
-      int a[]= new int [4];
-      a[0]=1;
-      a[1]=2;
-      a[2]=3;
-      a[3]=4;  
-      System.out.println(a[1]);
-      System.out.println();
-      for (int m=0;m<3;m++) {
-    	  System.out.println(a[m]);  }
-      System.out.println();
-      for (int j=3;j>=0;j--) {
-    	  System.out.println(a[j]);
-      }
-      System.out.println();
-
-      System.out.println(a.length);
-      
+    Scanner a= new Scanner(System.in);	 System.out.println("Your Name");
+    String b= a.nextLine();   System.out.println(b);
+    
+		
 	}
 
 }
